a5215800b9cddefb23f7b76afa2ae0b92c2608f1
James Wang <114769089+jameswang08@users.noreply.github.com>
2025-07-24 11:58:50 -0700

    Add script to preprocess baseline precipitation data

---
+++ preprocess_baseline.py
@@ -0,0 +1,39 @@
+# Script to convert bil files into NetCDF format for future processing.
+
+import os
+from glob import glob
+import rioxarray as rxr # type: ignore
+import xarray as xr # type: ignore
+
+root = 'Data/Baseline'
+output_dir = 'Data/BaselineCleaned'
+years = [d for d in os.listdir(root) if os.path.isdir(os.path.join(root, d))]
+
+for year in years:
+    year_path = os.path.join(root, year)
+    bil_files = glob(os.path.join(year_path, '*.bil'))
+
+    print(f"Processing {year}, found {len(bil_files)} bil files.")
+
+    for bil_file in bil_files:
+
+        # Assuming file structure like PRISM_ppt_stable_4kmM3_2018_all_bil where time is 4th element
+        filename = os.path.basename(bil_file)
+        file_date = filename.split('_')[4]
+
+        if len(file_date) != 6:
+            print(f"Skipping year summary file: {filename}")
+            continue
+
+        print(f"Processing {bil_file}")
+
+        ds = rxr.open_rasterio(bil_file)
+        ds.name = 'precipitation'
+
+        precip = ds.squeeze('band')
+        ds_clean = precip.to_dataset(name='precipitation')
+
+        final_nc = os.path.join(output_dir, f"{file_date}.nc")
+        ds_clean.to_netcdf(final_nc)
+
+        print(f"Saved processed NetCDF to {final_nc}")
