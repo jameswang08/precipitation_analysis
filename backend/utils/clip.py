a3de1bf1e21af4a4b9acf99d842f7e59ce608d82
James Wang <114769089+jameswang08@users.noreply.github.com>
2025-09-05 10:45:46 -0700

    Add script to clip baseline data outside of model data.

---
+++ backend/utils/clip.py
@@ -0,0 +1,15 @@
+import os
+
+directory = '../Data/BaselineCleaned'
+
+for filename in os.listdir(directory):
+    if filename.endswith('.nc'):
+        year_str = filename[:4]
+        try:
+            year = int(year_str)
+            if year < 1981 or year > 2019:
+                filepath = os.path.join(directory, filename)
+                print(f"Deleting {filepath}")
+                os.remove(filepath)
+        except ValueError:
+            pass
