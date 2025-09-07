e0bf136ef01abcc2a6a2e4317bb0dbf8825f7e61
James Wang <114769089+jameswang08@users.noreply.github.com>
2025-09-07 15:02:06 -0700

    Modify ACC calculation to properly use xarray's corr func.

--- backend/utils/metrics.py
+++ backend/utils/metrics.py
@@ -2,11 +2,12 @@ import xarray as xr
 import numpy as np
 
 def spatial_anomaly_correlation_coefficient(model, baseline, dim='date'):
-    model_anom = model - model.mean(dim=dim)
-    baseline_anom = baseline - baseline.mean(dim=dim)
+    return xr.corr(baseline, model, dim=dim)
 
-    numerator = (model_anom * baseline_anom).sum(dim=dim)
-    denominator = np.sqrt((model_anom ** 2).sum(dim=dim) * (baseline_anom ** 2).sum(dim=dim))
+def normalized_root_mean_square_error(difference, average, dim='year'):
+    rmse = np.sqrt(((difference) ** 2).mean(dim=dim))
+    return rmse / (average + 1e-10)
 
-    acc = xr.where(denominator != 0, numerator / denominator, np.nan)
-    return acc
+def normalized_mean_absolute_difference(difference, average, dim='year'):
+    mad = (np.abs(difference)).mean(dim=dim)
+    return mad / (average + 1e-10)
