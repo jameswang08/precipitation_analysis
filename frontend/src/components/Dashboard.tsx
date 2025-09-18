d26e51e7e510bd7458246940e38d2152dcdf13f7
James Wang <114769089+jameswang08@users.noreply.github.com>
2025-09-18 11:57:50 -0700

    Install leaflet and add skeleton code for switching display mode.

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -4,15 +4,47 @@ import WeatherForm from './WeatherForm';
 
 function Dashboard() {
   const [plotUrls, setPlotUrls] = useState<string[]>([]);
+  const [mapType, setMapType] = useState<string>('plots');
+
+  let content;
+
+  if (mapType === "plots") {
+    content =
+      (
+        <>
+          <WeatherForm setPlotUrls={setPlotUrls} />
+          <div className="Images">
+            {plotUrls.map((url, index) => (
+              <img key={index} src={url} alt={`Plot ${index + 1}`} />
+            ))}
+          </div>
+        </>
+      )
+  }
+  else {
+    content =
+      (
+        <div>
+          Hello World
+        </div>
+      )
+  }
 
   return (
     <div className="Dashboard">
-      <WeatherForm setPlotUrls={setPlotUrls} />
-      <div className="Images">
-        {plotUrls.map((url, index) => (
-          <img key={index} src={url} alt={`Plot ${index + 1}`} />
-        ))}
-      </div>
+      <button className = "toggleMode" onClick={
+        () => {
+          if (mapType === "plots"){
+            setMapType("interactive")
+          }
+          else {
+            setMapType("plots")
+          }
+        }
+      }>
+        Toggle Mode
+      </button>
+      {content}
     </div>
   );
 }
